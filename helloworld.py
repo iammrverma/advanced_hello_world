05030234e8df576b5a9bc62b6ce01c340cb47407
iammrverma <rajwebz2020@gmail.com>
2023-10-02 08:05:50 +0530

    final version

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,14 @@
-def print_word(string, character_index=0, printed_string=''):
-    for character in string:
-        for letter in range(ord('a'), ord('z') + 1):
-            print(printed_string + chr(letter))
-            if chr(letter) == character:
-                print_word(string[1:], character_index+1, printed_string + character)
+def print_word(string, printed_string=''):
+    if len(string) == 0:
+        return
+    if string[0] == " ":
+        string = string[1:]
+        printed_string += " "
+    for letter in range(ord('a'), ord(string[0]) + 1):
+        print(printed_string + chr(letter))
+        if string[0] == chr(letter):
+            print_word(string[1:], printed_string + chr(letter))
 
 
 if __name__ == '__main__':
-    print_word("ab")
+    print_word("hello world")
